53e7f0f3bba3e2b0f76bd41a761df1898a140c81
shashi-shas <shashimj.kumar@gmail.com>
2016-10-20 11:57:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/profile.png', function (req, res) {
+app.get('./ui/profile.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
 });
 
